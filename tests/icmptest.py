05ff06d97207c6896f0cf93cdffdd4632ea4b36f
Oddy <cubeman.op@gmail.com>
2024-12-10 21:17:28 +0200

    addit tracerout and dns resolve func

--- tests/icmptest.py
+++ tests/icmptest.py
@@ -1,20 +1,42 @@
 from scapy.all import *
 
-iface = "enp0s31f6"
-# Define the target IP address
-target_ip = "8.8.8.8"  # Example: Google Public DNS
 
-# Build the packet: IP layer + ICMP (ping)
-packet = IP(dst=target_ip)/ICMP()
-packet.show()
 
-# Send the packet and receive the response
-response = sr1(packet, timeout=2, iface=iface)
+def TraceRoute(target_ip,iface):
+	ttl = 1
+	while True:
 
-# Check if a response was received
-if response:
-    print("Received response from:", response[IP].src)
-    print("Response details:")
-    response.show()
-else:
-    print("No response received. The request may have timed out.")
+		print("\n\nhop "+str(ttl)+".")
+
+		# Build the packet: IP layer + ICMP (ping)
+		packet = IP(dst=target_ip, ttl=ttl)/ICMP()
+		print("request pkt: "+str(packet.summary()))
+
+		# Send the packet and receive the response
+		response = sr1(packet, timeout=2, iface=iface)
+
+		# Check if a response was received
+		if response:
+			print("Received response from:", response[IP].src)
+			print("response pkt: "+str(response.summary()))
+
+
+			# Check if target has ben reched
+			if target_ip == response[IP].src:
+				print("target reched!")
+				return 0
+			
+		
+		else:
+			print("No response received. The request may have timed out.")
+
+		ttl=ttl+1
+
+
+if __name__ == "__main__":
+	iface = "enp0s31f6"
+	# Define the target IP address
+	target_ip = "8.8.8.8"  # Example: Google Public DNS
+
+
+	TraceRoute(target_ip ,iface)
